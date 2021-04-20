207c4094f9b7a8307901da868be8df439ee93a6b
Masahiro Wada <argon.argon.argon@gmail.com>
2021-04-20 17:17:18 +0000

    add f1 score

--- src/shopee_product_matching/system.py
+++ src/shopee_product_matching/system.py
@@ -2,6 +2,7 @@ from dataclasses import asdict, dataclass
 from typing import List, Any, Dict
 
 import pytorch_lightning as pl
+from pytorch_lightning.core.lightning import LightningModule
 import torch
 from torch import nn
 import numpy as np
@@ -82,28 +83,28 @@ class ImageMetricLearning(pl.LightningModule):
 
     def validation_epoch_end(self, outputs: List[Dict[str, List[Any]]]) -> None:
         try:
-            flatten_outputs = self._accumulate_outputs(outputs)
-            infer_label_groups = get_image_predictions(
-                flatten_outputs["posting_ids"], flatten_outputs["embeddings"]
+            acc_outputs = _accumulate_outputs(outputs)
+            infer_matches = _get_image_predictions(
+                acc_outputs["posting_ids"], acc_outputs["embeddings"]
             )
 
-            df = pd.DataFrame(
-                {
-                    "label_group": flatten_outputs["label_groups"],
-                    "posting_id": flatten_outputs["posting_ids"],
-                }
+            expect_matches = _get_expect_matches(
+                acc_outputs["posting_ids"], acc_outputs["label_groups"]
             )
-            matches = df["label_group"].map(
-                df.groupby(["label_group"])["posting_id"].unique().to_dict()
-            )
-            intersection = np.array([(2 * len(set(a) & set(b))) / (len(a) + len(b)) for a,b in zip(matches, infer_label_groups)])
-            valid_f1_score = sum(intersection) / len(intersection)
+
+            valid_f1 = f1_score(infer_matches, expect_matches)
         except ValueError:
-            valid_f1_score = float('nan')
+            valid_f1 = float("nan")
 
         self.log(
-            "valid_f1_score", valid_f1_score, on_step=False, on_epoch=True, prog_bar=True, logger=True
+            "valid_f1",
+            valid_f1,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
         )
+
     def test_step(self, batch, batch_idx) -> Dict[str, Any]:
         x = batch[ShopeeProp.image]
         x.to(self.device)
@@ -111,49 +112,44 @@ class ImageMetricLearning(pl.LightningModule):
         return {"posting_id": posting_id, "embeddings": self(x)}
 
     def test_epoch_end(self, outputs: Dict[str, List[Any]]) -> None:
-        flatten_outputs = self._accumulate_outputs(outputs)
-        result = get_image_predictions(
-            flatten_outputs["posting_ids"], flatten_outputs["embeddings"]
+        acc_outputs = _accumulate_outputs(outputs)
+        matches = _get_image_predictions(
+            acc_outputs["posting_ids"], acc_outputs["embeddings"]
         )
-        df = pd.DataFrame(
-            {
-                "posting_id": flatten_outputs["posting_ids"],
-                "matches": [" ".join(rs) for rs in result]
-            }
+
+        _save_submission_csv(acc_outputs["posting_ids"], matches)
+
+
+def _accumulate_outputs(outputs: List[Dict[str, List[Any]]]) -> Dict[str, Any]:
+    embeddings = []
+    posting_ids = []
+    label_groups = []
+
+    for output in outputs:
+        embeddings.append(
+            [t.detach().cpu().numpy() for t in output.get("embeddings", [])]
         )
-        df.to_csv('submission.csv', index = False)
-        print(result)
-
-    def _accumulate_outputs(
-        self, outputs: List[Dict[str, List[Any]]]
-    ) -> Dict[str, Any]:
-        embeddings = []
-        posting_ids = []
-        label_groups = []
-
-        for output in outputs:
-            embeddings.append(
-                [t.detach().cpu().numpy() for t in output.get("embeddings", [])]
-            )
-            posting_ids.append(output.get("posting_id", []))
-            label_groups.append(
-                [t.detach().cpu().numpy() for t in output.get("label_group", [])]
-            )
-        return {
-            "embeddings": np.concatenate(embeddings),
-            "posting_ids": sum(posting_ids, []),
-            "label_groups": np.concatenate(label_groups),
-        }
+        posting_ids.append(output.get("posting_id", []))
+        label_groups.append(
+            [t.detach().cpu().numpy() for t in output.get("label_group", [])]
+        )
+    return {
+        "embeddings": np.concatenate(embeddings),
+        "posting_ids": sum(posting_ids, []),
+        "label_groups": np.concatenate(label_groups),
+    }
 
 
-def get_image_predictions(posting_ids, embeddings, threshold=3.4) -> List[List[str]]:
+def _get_image_predictions(
+    posting_ids: List[str], embeddings: np.ndarray, threshold: float = 3.4
+) -> List[List[str]]:
     KNN = min(3, len(posting_ids))
 
     model = NearestNeighbors(n_neighbors=KNN)
     model.fit(embeddings)
     distances, indices = model.kneighbors(embeddings)
 
-    predictions = []
+    predictions: List[List[str]] = []
     for k in range(embeddings.shape[0]):
         idx = np.where(
             distances[
@@ -165,3 +161,37 @@ def get_image_predictions(posting_ids, embeddings, threshold=3.4) -> List[List[s
         predictions.append([posting_ids[i] for i in ids])
 
     return predictions
+
+
+def f1_score(infer_matches: List[List[str]], expect_matches: List[List[str]]) -> float:
+    intersection = [
+        (2 * len(set(a) & set(b))) / (len(a) + len(b))
+        for a, b in zip(infer_matches, expect_matches)
+    ]
+    return sum(intersection) / len(intersection)
+
+
+def _save_submission_csv(posting_ids: List[str], matches: List[List[str]]):
+    df = pd.DataFrame(
+        {
+            "posting_id": posting_ids,
+            "matches": [" ".join(rs) for rs in matches],
+        }
+    )
+    df.to_csv("submission.csv", index=False)
+
+
+def _get_expect_matches(
+    posting_ids: List[str], label_groups: List[str]
+) -> List[List[str]]:
+    df = pd.DataFrame(
+        {
+            "label_group": label_groups,
+            "posting_id": posting_ids,
+        }
+    )
+    return (
+        df["label_group"]
+        .map(df.groupby(["label_group"])["posting_id"].unique().to_dict())
+        .values.tolist()
+    )
